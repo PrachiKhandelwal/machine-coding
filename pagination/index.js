23af7a50952f1f0048b3d0f8ef3c025d83ed8ddd
PrachiKhandelwal <prachik27200001@gmail.com>
2025-05-10 17:19:48 +0530

    completed pagination using react

--- pagination/index.js
+++ pagination/index.js
@@ -1,71 +1,67 @@
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    const productsContainer = document.createElement("div");
-    productsContainer.classList.add("products");
     let products = [];
     let page = 1;
-    let pageSize = 10;
-
+    let pageSize = 15;
     const fetchProducts = async () => {
         try {
-            const res = await fetch("https://dummyjson.com/products?limit=81");
+            const res = await fetch("https://dummyjson.com/products?limit=43");
             const data = await res.json();
-            if (data && data.products) {
-                products = data.products;
-                render();
-            }
+            products = data.products;
+            render();
         } catch (err) {
             console.log(err);
         }
     };
-
+    const productsContainer = document.createElement("div");
     const render = () => {
         productsContainer.innerHTML = "";
-        if (products.length) {
-            products
-                .slice(page * pageSize - pageSize, page * pageSize)
-                .forEach((product) => {
-                    const productItem = document.createElement("div");
-                    productItem.classList.add("products__single");
-                    productItem.innerHTML = `<img src="${product.thumbnail}"/><span>${product.title}</span>`;
-                    productsContainer.appendChild(productItem);
-                });
-        }
-        root.appendChild(productsContainer);
-        let totalPages = Math.ceil(products.length / pageSize);
+        let start = page * pageSize - pageSize;
+        let end = page * pageSize;
+        products.slice(start, end).forEach((product) => {
+            const productItem = document.createElement("div");
+            productItem.innerHTML = `<img src="${product.thumbnail}"/><p>${product.title}</p>`;
+            productsContainer.appendChild(productItem);
+        });
+        renderPaginator();
+    };
+
+    const renderPaginator = () => {
+        const totalPages = Math.ceil(products.length / pageSize);
         const paginator = document.createElement("div");
         if (page > 1) {
             const prevBtn = document.createElement("button");
             prevBtn.innerText = "Previous";
             prevBtn.addEventListener("click", () => {
-                page = page - 1;
+                page--;
                 render();
             });
             paginator.appendChild(prevBtn);
         }
         for (let i = 1; i <= totalPages; i++) {
-            const numberBtn = document.createElement("button");
-            numberBtn.innerText = i;
-            numberBtn.addEventListener("click", () => {
+            const btn = document.createElement("button");
+            btn.innerText = i;
+            btn.addEventListener("click", () => {
                 page = i;
                 render();
             });
-            paginator.appendChild(numberBtn);
+            if (i === page) {
+                btn.style.backgroundColor = "skyblue";
+                btn.style.color = "white";
+            }
+            paginator.appendChild(btn);
         }
         if (page < totalPages) {
             const nextBtn = document.createElement("button");
             nextBtn.innerText = "Next";
             nextBtn.addEventListener("click", () => {
-                page = page + 1;
+                page++;
                 render();
             });
             paginator.appendChild(nextBtn);
         }
-        const pageDesc = document.createElement("span");
-        pageDesc.innerText = `Page ${page} of ${totalPages}`;
-        paginator.appendChild(pageDesc);
         productsContainer.appendChild(paginator);
     };
-
+    root.appendChild(productsContainer);
     fetchProducts();
 });
